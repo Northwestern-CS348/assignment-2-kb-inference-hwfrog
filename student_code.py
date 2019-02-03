931a7ca2d67e21bc7da4146a4d42241a018f6b7a
HW <374154361@qq.com>
2019-02-02 19:36:12 -0600

    fixed bug in retract

--- student_code.py
+++ student_code.py
@@ -116,6 +116,34 @@ class KnowledgeBase(object):
             print("Invalid ask:", fact.statement)
             return []
 
+    def retract_fact(self, fact):
+        fact = self._get_fact(fact)
+        if not fact.asserted and not fact.supported_by:
+            for pair in fact.supported_by:
+                for fr in pair:
+                    fr.supports_facts.remove(fact)
+            for f in fact.supports_facts:
+                f.supported_by = [pair for pair in f.supported_by if pair[0]!=fact]
+                self.retract_fact(f)
+            for r in fact.supports_rules:
+                r.supported_by = [pair for pair in r.supported_by if pair[0]!=fact]
+                self.retract_rule(r)
+            self.facts.remove(fact)            
+
+    def retract_rule(self, rule):
+        rule = self._get_rule(rule)
+        if not rule.asserted and not rule.supported_by:
+            for pair in rule.supported_by:
+                for fr in pair:
+                    fr.supports_rules.remove(rule)
+            for f in rule.supports_facts:
+                f.supported_by = [pair for pair in f.supported_by if pair[1]!=rule]
+                self.retract_fact(f)
+            for r in rule.supports_rules:
+                r.supported_by = [pair for pair in r.supported_by if pair[1]!=rule]
+                self.retract_rule(r)
+            self.rules.remove(rule)
+
     def kb_retract(self, fact_or_rule):
         """Retract a fact from the KB
 
@@ -131,31 +159,11 @@ class KnowledgeBase(object):
 
         if isinstance(fact_or_rule, Fact):
             fact = self._get_fact(fact_or_rule)
-            if not fact.supported_by:
-                for pair in fact.supported_by:
-                    for fr in pair:
-                        fr.supports_facts.remove(fact)
-                for f in fact.supports_facts:
-                    f.supported_by = [pair for pair in f.supported_by if pair[0]!=fact]
-                    self.kb_retract(f)
-                for r in fact.supports_rules:
-                    r.supported_by = [pair for pair in r.supported_by if pair[0]!=fact]
-                    self.kb_retract(r)
-                self.facts.remove(fact)
-
+            if fact.asserted: fact.asserted = False
+            self.retract_fact(fact)
         elif isinstance(fact_or_rule, Rule):
             rule = self._get_rule(fact_or_rule)
-            if not rule.asserted:
-                for pair in rule.supported_by:
-                    for fr in pair:
-                        fr.supports_rules.remove(rule)
-                for f in rule.supports_facts:
-                    f.supported_by = [pair for pair in f.supported_by if pair[1]!=rule]
-                    self.kb_retract(f)
-                for r in rule.supports_rules:
-                    r.supported_by = [pair for pair in r.supported_by if pair[1]!=rule]
-                    self.kb_retract(r)
-                self.rules.remove(rule)
+            self.retract_rule(rule)
 
 
 
